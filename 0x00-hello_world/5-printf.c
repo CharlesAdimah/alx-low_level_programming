6e0334e6856fe710c88bbbc82e1cb8fb16299a46
CharlesAdimah <charlesosejob@gmail.com>
2022-09-08 21:06:14 -0700

    5. Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,12 @@
-#include <stdio.h>                                                                                                                      
+#include <stdio.h>
 
-/**                                                                                                                                     
- * main - Print the phrase with proper grammar...                                                                                        *
- * Return: Always 0 (if print is successful)
- */                                                                                                                                     
-int main(void)                                                                                                                          
+/**
+ * main - Print the phrase with proper grammer...
+ *
+ * Return: Always 0 (If print is successful)
+ */
+int main(void) 
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");                                                                   
-	return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return(0);
 }
