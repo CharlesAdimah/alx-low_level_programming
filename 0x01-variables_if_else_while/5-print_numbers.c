c8e74f380c021e67272117807fd24bc2e97ee833
CharlesAdimah <charlesosejob@gmail.com>
2022-09-09 18:51:05 -0700

    5. Nubers 0 to 9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - prints single base 10 digits 
+ * starting with 0
+ * Return: Always 0 (When successful)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+		printf("%d", n);
+	printf("\n");
+	return (0);
+}
