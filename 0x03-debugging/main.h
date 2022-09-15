6b6230e96f701dd6fd55750c6b0d70707b9b6f1a
CharlesAdimah <charlesosejob@gmail.com>
2022-09-15 12:34:38 -0700

    0. Test function

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -3,9 +3,6 @@
 
 #include <stdio.h>
 
-void positive_or_negative(int i);
-int largest_number(int, int, int);
-void print_remaining _days(int, int, int);
-int convert_day(int, int);
+void positive_or_negative(int i)
 
 #endif /* MAIN_H */
