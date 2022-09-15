1816da1afb794fb8c3b82020313a796bc1fc6907
CharlesAdimah <charlesosejob@gmail.com>
2022-09-15 12:23:03 -0700

    0. Test function

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -4,13 +4,10 @@
  * main - create test fuction that prints if integer is positive or negative
  * Return: 0 (successful)
  */
-
 int main(void)
 {
 	int i;
-
 	i = 98;
 	positive_or_negative(i);
-
 	return (0);
 }
