7f9b81024df5e59405b845b6ee36ba01a1254ed9
CharlesAdimah <charlesosejob@gmail.com>
2022-09-19 19:44:49 -0700

    2.Get string length

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -8,10 +8,10 @@
 
 int _strlen(char *s)
 {
-	int length = 0;
+	int len = 0;
 
-	while (*(s + lenght) != '\0')
-		length++;
+	while (*(s + len) != '\0')
+		len++;
 
-	return (length);
+	return (len);
 }
