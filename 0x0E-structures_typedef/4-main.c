513c80bcd6914f03cfb06fd9f34dd82ba812e725
CharlesAdimah <charlesosejob@gmail.com>
2022-10-10 12:57:21 -0700

    4. New dog

--- 0x0E-structures_typedef/4-main.c
+++ 0x0E-structures_typedef/4-main.c
@@ -8,9 +8,9 @@
  */
 int main(void)
 {
-	dog_t *new_dog;
+	dog_t *my_dog;
 
-	*new_dog = new_dog("Poppy", 3.5, "Bob");
-	printf("My name is %s, and I am %.1f :) - Woof!\n", new_dog->name, new_dog->age);
+	my_dog = new_dog("Poppy", 3.5, "Bob");
+	printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog->name, my_dog->age);
 	return (0);
 }
